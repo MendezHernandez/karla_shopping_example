c27a9b56835ddd3e5e4f6310abd8b58a3e87ad44
Jonathan Mendez <mendezhernandez999@gmail.com>
2025-04-07 18:43:09 -0700

    add error page content

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -7,6 +7,16 @@ const Error = () => {
     <>
       <Header />
       <Navbar />
+      <div>
+        <h2>🌸 Página no encontrada</h2>
+        <p style={{ fontSize: "larger" }}>
+          La fragancia que buscas parece haberse desvanecido en el aire. Puede
+          que el enlace esté roto o la página haya sido retirada de nuestra
+          colección. Te invitamos a regresar al inicio y descubrir nuestras
+          esencias más exclusivas.
+        </p>
+        <a href="/">Volver al inicio</a>
+      </div>
     </>
   );
 };
